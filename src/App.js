aa620ecd7c9fae00e34fec89aadcb0840a1ef71e
ruims <2218466341@qq.com>
2021-08-13 08:32:49 +0800

    feat: 添加版本1.0.2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          看到这个界面，说明 docker 部署成功了！！
-          版本：1.0.1
-        </p>
+        <p>看到这个界面，说明 docker 部署成功了！！ 版本：1.0.2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
